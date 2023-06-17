d8e9ea4249b9cc3863dee602760f5710acbe93b4
Dinesh <noreply@github.com>
2023-06-17 07:53:59 +0530

    Update workers to 4

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-gunicorn -k uvicorn.workers.UvicornWorker --workers 3 --bind 0.0.0.0:8000 shareaichat.asgi:application
+gunicorn -k uvicorn.workers.UvicornWorker --workers 4 --bind 0.0.0.0:8000 shareaichat.asgi:application
