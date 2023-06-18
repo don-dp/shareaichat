c555dd4e22e62ee24bdd8e16f83be477871176d2
Dinesh <noreply@github.com>
2023-06-18 18:02:52 +0530

    Add post model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at', 'updated_at')
+    search_fields = ('title', 'content',)
+    list_filter = ('created_at',)
+    date_hierarchy = 'created_at'
+
+admin.site.register(Post, PostAdmin)
